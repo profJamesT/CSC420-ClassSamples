3522de9e9ed2412361a8fd6302d213a0b2cebebc
jamestucker <jtucker@csp.edu>
2020-01-15 22:59:59 -0500

    Initial Load

---
+++ src/edu/csp/csc420/week1/DemoMain.java
@@ -0,0 +1,12 @@
+package edu.csp.csc420.week1;
+
+/**
+ * @author James Tucker - jtucker@csp.edu
+ */
+
+public class DemoMain {
+    public static void main(String[] args) {
+        System.out.println("This is the demo file for week 1");
+    }
+}
+
